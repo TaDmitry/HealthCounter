d377a0e7c4f9e2a8228acf4b1af04d1a8e89bae1
TaDmitry <tarasenkodmitriy000@gmail.com>
2024-09-24 00:28:01 +0500

    Не значительная работа с visual

--- visual/src/App.tsx
+++ visual/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./views/Header";
+import Header from "./views/HeaderApp";
 
 const App: React.FC = () => {
 	return (
